9095e1a710eb4ed4aba8db30083d5e30bef75163
Prasad22V <prasadvaidya20818@gmail.com>
2024-10-29 10:03:57 +0530

    cors updated

--- app.js
+++ app.js
@@ -16,22 +16,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
-
-// Manually setting CORS headers
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    process.env.FRONTEND_URL || "*"
-  );
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
-
-// Handling preflight requests
-
-
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  })
+);
 
 // importing & using routes
 import course from "./routes/courseRoute.js";
